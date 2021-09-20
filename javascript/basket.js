b73a9a4574f6b8b47a80856aa8a2b6bb18b97aab
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-09-21 01:28:53 +0200

    Added the price format

--- javascript/basket.js
+++ javascript/basket.js
@@ -35,12 +35,14 @@ function showProduct() {
 	let basketItems = localStorage.getItem('basketItems');
 	basketItems = JSON.parse(basketItems);
 
-	console.log(basketItems);
+	// console.log(basketItems);
 
 	if (basketItems && row) {
 		// row.innerHTML = "";
 		Object.values(basketItems).map(item => {
-
+			let price =
+				`${item.price}`.substring(0, `${item.price}`.length - 2) +
+				',' + `${item.price}`.substring(`${item.price}`.length - 2);
 			row.innerHTML +=
 				`<div class="col-12 col-md-3 mt-3 mx-sm-0 mx-0 text-center">
                         <img class="rounded-0" src="${item.imageUrl}" height="112" alt="">
@@ -51,9 +53,9 @@ function showProduct() {
                                 <tr><th scope="col">Nom</th><th scope="col">Quantité</th><th scope="col">Prix</th><th scope="col">Lentille</th> </tr>
                             </thead>
                             <tbody>
-                                <tr><td>${item.name}</td><td>${item.quantity}</td><td>${item.price} €</td><td>${item.lenses[0]}</td></tr>
+                                <tr><td>${item.name}</td><td>${item.quantity}</td><td>${price} €</td><td>${item.lenses[0]}</td></tr>
                             <tfoot>
-                                <tr><td class="text-info">Total</td><td></td><td></td><td class="text-info">${item.quantity * item.price} €</td></tr>
+                                <tr><td class="text-info">Total</td><td></td><td></td><td class="text-info">${item.quantity * parseInt(price)},00 €</td></tr>
                             </tfoot>
                         </table>
                         <div class="d-flex justify-content-center justify-content-md-end">
@@ -65,8 +67,14 @@ function showProduct() {
 		row.innerHTML = `<h2 class="text-center text-secondary">Votre panier est vide !<a href="index.html" class="text-info"> Retour à l'accueil</a></h2>`;
 	}
 
-	let totalCost = localStorage.getItem('totalCost')
+	let totalCost = localStorage.getItem('totalCost');
+
+	
 	if (totalCost) {
+		totalCost =
+			`${totalCost}`.substring(0, `${totalCost}`.length - 2) +
+			',' +
+			`${totalCost}`.substring(`${totalCost}`.length - 2);
 		row.innerHTML +=
 		`<div class=" mt-5 d-flex justify-content-between text-center border-top border-dark">
 			<div class="text-secondary"><h4>Total de la commande :</h4></div>
