8346193894f927c7553b2d907cbb6e937f86adb5
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-09-27 23:23:09 +0200

    The if statement has been modified

--- javascript/order-confirm.js
+++ javascript/order-confirm.js
@@ -4,16 +4,25 @@ let orderId = new URL(location.href).searchParams.get('id');
 // Sets order Id to the confiramtion page
 function assignOrderId() {
 
-    document.querySelector('#orderId').innerText = orderId;
-    
-    let totalCost = localStorage.getItem('totalCost');
-    totalCost =
-		`${totalCost}`.substring(0, `${totalCost}`.length - 2) +
-		',' +
-        `${totalCost}`.substring(`${totalCost}`.length - 2);
-    
-    document.querySelector('#totalPrice').innerText = `${totalCost} €`
+    if (!localStorage.getItem('basketItems')) {
+        document.querySelector('.order-confirm-container')
+            .style.display = 'none';
+         document.querySelector('.backword_message')
+            .innerHTML = `<h2 class="text-center text-secondary">Retour <a href="index.html" class="text-info">à la page d'accueil !</a></h2>`;
+    } else {
+        document.querySelector('.order-confirm-container').style.display =
+            'block';
+        document.querySelector('#orderId').innerText = orderId;
 
+        let totalCost = localStorage.getItem('totalCost');
+        totalCost =
+            `${totalCost}`.substring(0, `${totalCost}`.length - 2) +
+            ',' +
+            `${totalCost}`.substring(`${totalCost}`.length - 2);
+
+        document.querySelector('#totalPrice').innerText = `${totalCost} €`;
+        localStorage.clear();
+    } 
 }
 
 
@@ -28,8 +37,10 @@ function setCurrentDate() {
     let minut = date.getMinutes();
 
     let fullDate = `Le ${day}/${month}/${year} à ${hour}h${minut}`;
-    document.querySelector('#dateOfPurchase').innerText = `Date de commande ${fullDate}`;
+    document.querySelector('#dateOfPurchase').innerText = `Date de commande : ${fullDate}`;
+
     
+
 }
 setCurrentDate();
 assignOrderId();
